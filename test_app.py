ede3545f2203273004f7fd3031bab5c76f717fa7
root <root@ILYAPC.>
2026-03-05 16:21:05 +0300

    broken test

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from app import add, subtract
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 99  # специально неправильно
 
 def test_subtract():
     assert subtract(5, 3) == 2
